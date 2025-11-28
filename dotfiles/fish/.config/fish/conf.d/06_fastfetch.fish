b06c38e79cc89c784e4ad90f692fb8684b63028c
Nikita Mitasov <me@ch4og.com>
2025-11-29 00:06:37 +0300

    dotfiles: fish: display fastfetch on ghostty too

--- dotfiles/fish/.config/fish/conf.d/06_fastfetch.fish
+++ dotfiles/fish/.config/fish/conf.d/06_fastfetch.fish
@@ -1,6 +1,6 @@
 if status is-interactive
-    if string match -q "*kitty*" $TERM
-        if test "$SHLVL" -eq 1 && test -z "$IN_NIX_SHELL" && test -z "$GUIX_ENVIRONMENT"
+    if string match -q "*ghostty*" $TERM || string match -q "*kitty*" $TERM
+        if test "$SHLVL" -eq 1
             command -v fastfetch >/dev/null 2>&1 && fastfetch
         end
     end
