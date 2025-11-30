266bde386389fc29ddcbffab9e50fc91049c2583
Nikita Mitasov <me@ch4og.com>
2025-11-30 15:50:06 +0300

    repo: add emacs repo configs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;; SPDX-FileCopyrightText: 2025 Nikita Mitasov <me@ch4og.com>
+;; SPDX-License-Identifier: MIT
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 2)))
+ (scheme-mode . ((indent-tabs-mode . nil)))
+(emacs-lisp-mode . ((indent-tabs-mode . nil))))
