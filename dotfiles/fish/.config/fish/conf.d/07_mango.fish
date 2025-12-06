4f43086c3dd39b568a62ffd09327cf3e7e1e1ab6
Nikita Mitasov <me@ch4og.com>
2025-12-06 03:57:14 +0300

    dotfiles: fish: fix autostarting mango on ssh

--- dotfiles/fish/.config/fish/conf.d/07_mango.fish
+++ dotfiles/fish/.config/fish/conf.d/07_mango.fish
@@ -1,4 +1,4 @@
-if status is-interactive && test -z "$WAYLAND_DISPLAY" && test "$(cat /sys/class/tty/tty0/active)" = "tty1"
+if status is-interactive && test -z "$WAYLAND_DISPLAY" && test "$(cat /sys/class/tty/tty0/active)" = "tty1" && test -z "$SSH_CONNECTION"
     if set -q KMS_START_SCRIPT
         exec $KMS_START_SCRIPT mango
     else
