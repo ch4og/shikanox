9cda861b17b4f4ef86f5437d68ba3697a85620b1
Nikita Mitasov <me@ch4og.com>
2025-12-07 09:38:58 +0300

    dotfiles: gtk: set theme finally

--- guix/shika/home/config.scm
+++ guix/shika/home/config.scm
@@ -52,6 +52,7 @@
                                                                            (packages '
                                                                             ("fastfetch"
                                                                              "dxvk"
+                                                                             "gtk"
                                                                              "ghostty"
                                                                              "mangowc"
                                                                              "emacs"
