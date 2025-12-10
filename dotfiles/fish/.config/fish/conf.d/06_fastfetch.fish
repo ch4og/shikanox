65e80bf5415652fcc2bf19f54b84f0943da3e83b
Nikita Mitasov <me@ch4og.com>
2025-12-10 19:42:26 +0300

    dotfiles: fish: fix fastfesch

--- dotfiles/fish/.config/fish/conf.d/06_fastfetch.fish
+++ dotfiles/fish/.config/fish/conf.d/06_fastfetch.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if string match -q "*ghostty*" $TERM || string match -q "*kitty*" $TERM
-        if test "$SHLVL" -eq 2
+        if test "$SHLVL" -eq 1
             fastfetch -c ~/.config/fastfetch/autorun.jsonc
         end
     end
