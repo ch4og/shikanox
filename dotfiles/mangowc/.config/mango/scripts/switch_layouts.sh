028d99670f0221fd37d13172114514afbd59d4ab
Nikita Mitasov <me@ch4og.com>
2025-12-20 04:33:02 +0300

    dotfiles: mango: make switch layout script safer and more reliable

--- dotfiles/mangowc/.config/mango/scripts/switch_layouts.sh
+++ dotfiles/mangowc/.config/mango/scripts/switch_layouts.sh
@@ -1,16 +1,44 @@
 #!/usr/bin/env bash
 
-targets="starrail.exe"
-declare -A state
+TARGET_APPS=(
+    "starrail.exe"
+)
 
-mmsg -w | while read monitor field value; do
-    [ "$field" = "appid" ] || continue
-    prev="${state[$monitor]}"
-    if echo "$value" | grep -qE "^($targets)$"; then
-        [ "$prev" != "in" ] && mmsg -d "setoption,xkb_rules_variant,"
-        state[$monitor]="in"
-    else
-        [ "$prev" = "in" ] && mmsg -d "setoption,xkb_rules_variant,colemak,"
-        state[$monitor]="out"
+declare -A APPID_BY_MONITOR
+CURRENT_LAYOUT="default"
+LOCKFILE="/tmp/layout_switch.lock"
+
+is_target_app() {
+    local target
+
+    for target in "${TARGET_APPS[@]}"; do
+        if [ "$1" = "$target" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+handle_focus_change() {
+    if is_target_app "${APPID_BY_MONITOR[$1]}"; then
+        if [ "$CURRENT_LAYOUT" != "game" ]; then
+            mmsg -d "setoption,xkb_rules_variant,"
+            CURRENT_LAYOUT="game"
+        fi
+    elif [ "$CURRENT_LAYOUT" = "game" ]; then
+        mmsg -d "setoption,xkb_rules_variant,colemak,"
+        CURRENT_LAYOUT="default"
+    fi
+}
+
+exec 204>"$LOCKFILE"
+flock -n 204 || { echo "Another instance is running"; exit 1; }
+
+mmsg -w | while read -r monitor event value; do
+    if [ "$event" = "appid" ]; then
+        APPID_BY_MONITOR["$monitor"]="$value"
+    elif [ "$event" = "selmon" ] && [ "$value" = "1" ]; then
+        handle_focus_change "$monitor"
     fi
 done
