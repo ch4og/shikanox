c8700d97912f37afadf91cadf2695689916f3335
Nikita Mitasov <me@ch4og.com>
2025-12-20 04:50:05 +0300

    system: packages: add fonts and nss-certs

--- guix/shika/system/packages.scm
+++ guix/shika/system/packages.scm
@@ -4,10 +4,22 @@
 (define-module (shika system packages)
   #:use-module (gnu)
   #:use-module (gnu packages docker)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages nss)
   #:use-module (gnu packages shells)
   #:use-module (gnu packages ssh)
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages vim))
 
 (define-public %shika-system-packages
-  (cons* vim fish openssh git docker %base-packages))
+  (cons* vim
+         fish
+         openssh
+         git
+         docker
+         font-hack
+         font-google-noto
+         font-google-noto-emoji
+         font-google-noto-sans-cjk
+         nss-certs
+         %base-packages))
