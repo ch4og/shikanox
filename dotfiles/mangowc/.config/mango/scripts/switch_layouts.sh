7de015cce216e1bb953e7d557864923df2547c3a
Nikita Mitasov <me@ch4og.com>
2025-12-27 05:32:34 +0300

    dotfiles: mango: update switch layouts script

--- dotfiles/mangowc/.config/mango/scripts/switch_layouts.sh
+++ dotfiles/mangowc/.config/mango/scripts/switch_layouts.sh
@@ -8,17 +8,15 @@ TARGET_APPS=(
 declare -A APPID_BY_MONITOR
 CURRENT_LAYOUT="default"
 LOCKFILE="/tmp/layout_switch.lock"
+LAST_APPID=""
 
 is_target_app() {
     local appid="$1"
-    local target
-
     for target in "${TARGET_APPS[@]}"; do
-        if [[ "$appid" =~ "$target" ]]; then
+        if [[ "$appid" =~ $target ]]; then
             return 0
         fi
     done
-
     return 1
 }
 
@@ -28,22 +26,39 @@ handle_focus_change() {
 
     if is_target_app "$appid"; then
         if [ "$CURRENT_LAYOUT" != "game" ]; then
-            mmsg -d "setoption,xkb_rules_variant,"
+            echo "Switching layout to 'game' for app '$appid' on monitor '$monitor'..."
+            mmsg -d "setoption,xkb_rules_variant,,"
+            mmsg -d "setoption,xkb_rules_variant,,"
+            mmsg -d "setoption,xkb_rules_variant,,"
+            mmsg -d "setoption,xkb_rules_variant,,"
             CURRENT_LAYOUT="game"
         fi
     elif [ "$CURRENT_LAYOUT" = "game" ]; then
+        echo "Switching back to 'default' layout on monitor '$monitor'..."
+        mmsg -d "setoption,xkb_rules_variant,colemak,"
+        mmsg -d "setoption,xkb_rules_variant,colemak,"
+        mmsg -d "setoption,xkb_rules_variant,colemak,"
         mmsg -d "setoption,xkb_rules_variant,colemak,"
         CURRENT_LAYOUT="default"
     fi
+
+    LAST_APPID="$appid"
 }
 
 exec 204>"$LOCKFILE"
 flock -n 204 || { echo "Another instance is running"; exit 1; }
 
 mmsg -w | while read -r monitor event value; do
-    if [ "$event" = "appid" ]; then
-        APPID_BY_MONITOR["$monitor"]="$value"
-    elif [ "$event" = "selmon" ] && [ "$value" = "1" ]; then
-        handle_focus_change "$monitor"
-    fi
+    case "$event" in
+        "appid")
+            APPID_BY_MONITOR["$monitor"]="$value"
+            ;;
+        "selmon")
+            if [ "$value" = "1" ]; then
+                if [ "${APPID_BY_MONITOR[$monitor]}" != "$LAST_APPID" ]; then
+                    handle_focus_change "$monitor"
+                fi
+            fi
+            ;;
+    esac
 done
