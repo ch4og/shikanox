565b835b3d76b4e0df34f0b3cb85366c77155e6c
Nikita Mitasov <me@ch4og.com>
2026-09-14 03:17:45 +0300

    shika: Add librewolf-sidebar-chatbot.
    * shika/packages/browsers.scm (librewolf-sidebar-chatbot): New variable.

---
+++ shika/packages/browsers.scm
@@ -0,0 +1,56 @@
+;;; SPDX-FileCopyrightText: 2026 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (shika packages browsers)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix gexp)
+  #:use-module (guix packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages librewolf))
+
+(define-public librewolf-sidebar-chatbot
+  (package
+    (name "librewolf-sidebar-chatbot")
+    (version (package-version librewolf))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments
+     (list
+      #:modules '((guix build utils))
+      #:builder
+      #~(begin
+          (use-modules (guix build utils))
+          (let* ((input #$(this-package-input "librewolf"))
+                 (sed (string-append #$(this-package-native-input "sed")
+                                     "/bin/sed"))
+                 (browser (string-append #$output "/lib/librewolf"))
+                 (launcher (string-append browser "/librewolf"))
+                 (policy (string-append browser "/distribution/policies.json"))
+                 (desktop (string-append #$output
+                                         "/share/applications/librewolf.desktop")))
+            (copy-recursively input #$output
+                              #:copy-file
+                              (lambda (source destination)
+                                (if (member (basename source)
+                                            '("librewolf"
+                                              ".librewolf-real"
+                                              "policies.json"
+                                              "librewolf.desktop"))
+                                    (copy-file source destination)
+                                    (symlink source destination))))
+            (substitute* (list launcher desktop)
+              ((input) #$output))
+            (invoke sed "-i" "/SidebarChatbot/,/Value/s/blocked/available/" policy)
+            (delete-file (string-append #$output "/bin/librewolf"))
+            (symlink launcher (string-append #$output "/bin/librewolf"))))))
+    (inputs (list librewolf))
+    (native-inputs (list sed))
+    (home-page (package-home-page librewolf))
+    (synopsis
+     (string-append (package-synopsis librewolf)
+                    " with the Sidebar Chatbot enabled"))
+    (description
+     (string-append (package-description librewolf)
+                    "  This variant enables Sidebar Chatbot through distribution
+policy."))
+    (license (package-license librewolf))))
